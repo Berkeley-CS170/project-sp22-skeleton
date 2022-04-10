cb936483de6924ee03f3a309bf116a48003ba98b
Rahul Arya <rahularya@berkeley.edu>
2022-04-09 23:32:05 -0700

    Update base for Update on "Add `make visualize` command"
    [ghstack-poisoned]

--- python/generate.py
+++ python/generate.py
@@ -8,7 +8,6 @@ For usage, run `python3 generate.py --help`.
 import argparse
 from pathlib import Path
 
-from cli_utils import StdoutFileWrapper
 from instance import Instance
 from size import Size
 from file_wrappers import StdoutFileWrapper
