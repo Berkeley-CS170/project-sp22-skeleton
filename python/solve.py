8cb9325a78ff2a37ad3b02f695654af6b48823e3
Thomas <52986112+tomtominator@users.noreply.github.com>
2022-04-15 16:34:31 -0700

    Specify output file in solve.py.
    By default, Windows encodes shell output in UTF-16, but we want to use UTF-8.
    
    Co-authored-by: Rahul Arya <rahularya@berkeley.edu>

--- python/solve.py
+++ python/solve.py
@@ -12,7 +12,7 @@ from typing import Callable, Dict
 
 from instance import Instance
 from solution import Solution
-from file_wrappers import StdinFileWrapper
+from file_wrappers import StdinFileWrapper, StdoutFileWrapper
 
 
 def solve_naive(instance: Instance) -> Solution:
@@ -34,6 +34,11 @@ def infile(args):
 
     return Path(args.input).open("r")
 
+def outfile(args):
+    if args.output == "-":
+        return StdoutFileWrapper()
+
+    return Path(args.output).open("w")
 
 def main(args):
     with infile(args) as f:
@@ -41,8 +46,9 @@ def main(args):
         solver = SOLVERS[args.solver]
         solution = solver(instance)
         assert solution.valid()
-        print("# Penalty: ", solution.penalty(), file=sys.stdout)
-        solution.serialize(sys.stdout)
+        with outfile(args) as g:
+            print("# Penalty: ", solution.penalty(), file=g)
+            solution.serialize(g)
 
 
 if __name__ == "__main__":
@@ -51,4 +57,7 @@ if __name__ == "__main__":
                         "read an instance from. Use - for stdin.")
     parser.add_argument("--solver", required=True, type=str,
                         help="The solver type.", choices=SOLVERS.keys())
+    parser.add_argument("output", type=str, 
+                        help="The output file. Use - for stdout.", 
+                        default="-")
     main(parser.parse_args())
